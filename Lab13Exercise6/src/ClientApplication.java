64c346a3ccafd17d9f8b3a59f27fb32de852b16b
skyrizer <wafir2002@gmail.com>
2023-06-14 14:41:58 +0800

    exercise 6 lab 13

---
+++ Lab13Exercise6/src/ClientApplication.java
@@ -0,0 +1,54 @@
+
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		
+		ClientFrame clientFrame = new ClientFrame();
+		String text = "Good Morning";
+		String targetLanguage = "Bahasa Melayu";
+		
+		try {
+			
+		// Launch client-side frame
+		
+		clientFrame.setVisible(true);
+			
+		// Connect to the server @ localhost, port 1234
+		Socket socket = new Socket(InetAddress.getLocalHost(), 1234);
+
+		// Update the status of the connection
+		clientFrame.updateConnectionStatus(socket.isConnected());
+		System.out.println(socket.isConnected());
+
+		// Write text for server
+		PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+		
+		// send text
+		printWriter.println(text);
+		printWriter.println(targetLanguage);
+		System.out.println("text send to server");
+		
+		// Read from network
+		BufferedReader bufferedReader = new BufferedReader
+				(new InputStreamReader(socket.getInputStream()));
+
+		// display translated words
+		String translated = bufferedReader.readLine();
+		clientFrame.translatedWord(translated);
+	
+
+		// Close everything
+		bufferedReader.close();
+		socket.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
